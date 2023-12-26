70bef848a6ea3e550ffd226876252f9afb41ef67
Kirill Martynov <kir.mart.vich@gmail.com>
2023-12-27 02:30:00 +0300

    Add basic framer-motion animation for CalendarWidget

--- src/features/Main/components/CalendarWidget/CalendarWidget.tsx
+++ src/features/Main/components/CalendarWidget/CalendarWidget.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { add, eachDayOfInterval, format, getDay, isSameMonth, parse, startOfDay } from 'date-fns';
+import { AnimatePresence, motion } from 'framer-motion';
 import cn from 'classnames';
 
 import { Button } from '@ui/Button';
@@ -44,14 +45,14 @@ export const CalendarWidget = ({ className }: CalendarWidgetProps) => {
     <Widget className={cn(s.root, className)}>
       <div className={s.header}>
         <Button className={s.button} theme="secondary" onClick={handlePreviousMonth}>
-          <Svg name="chevron" width={20} height={20} />
+          <Svg name="chevron" width={18} height={18} />
         </Button>
         <div className={s.title}>
           <div className={s.month}>{format(firstDayCurrentMonth, 'MMMM')}</div>
           <div className={s.year}>{format(firstDayCurrentMonth, 'yyyy')}</div>
         </div>
         <Button className={s.button} theme="secondary" onClick={handleNextMonth}>
-          <Svg name="chevron" width={20} height={20} />
+          <Svg name="chevron" width={18} height={18} />
         </Button>
       </div>
       <div className={s.days}>
@@ -63,20 +64,34 @@ export const CalendarWidget = ({ className }: CalendarWidgetProps) => {
         <div>Sat</div>
         <div>Sun</div>
       </div>
-      <div className={s.content}>
-        {days.map((day) => (
-          <div
-            key={day.toString()}
-            className={cn(s.day, {
-              [s.today]: day.getTime() === today.getTime(),
-              [s.otherMonth]: !isSameMonth(day, today),
-            })}
-            style={{ gridColumnStart: getDay(day) === 0 ? 7 : getDay(day) }}
-          >
-            <time dateTime={format(day, 'yyyy-MM-dd')}>{format(day, 'd')}</time>
-          </div>
-        ))}
-      </div>
+      <AnimatePresence mode="popLayout" initial={false}>
+        <motion.div
+          className={s.content}
+          key={currentMonth}
+          initial="enter"
+          animate="middle"
+          exit="exit"
+          variants={{
+            enter: { x: '100%' },
+            middle: { x: '0%' },
+            exit: { x: '-100%' },
+          }}
+          transition={{ type: 'tween' }}
+        >
+          {days.map((day) => (
+            <div
+              key={day.toString()}
+              className={cn(s.day, {
+                [s.today]: day.getTime() === today.getTime(),
+                [s.otherMonth]: !isSameMonth(day, today),
+              })}
+              style={{ gridColumnStart: getDay(day) === 0 ? 7 : getDay(day) }}
+            >
+              <time dateTime={format(day, 'yyyy-MM-dd')}>{format(day, 'd')}</time>
+            </div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
     </Widget>
   );
 };
